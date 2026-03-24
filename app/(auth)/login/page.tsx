01fe30ed31f9325dbe98f25c5f66b93ca32b4b5a
Dharm Thakkar <dharm.codes@gmail.com>
2026-03-24 09:57:08 +0530

    ...

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -32,12 +32,11 @@ export default function LoginPage() {
       },
       body: JSON.stringify(data),
     });
-    
+
     if (res.ok) {
       refresh();
       console.log("user after refresh", user);
       router.push("/onboarding");
-
     } else {
       const error = await res.json();
       console.log(error);
@@ -58,64 +57,72 @@ export default function LoginPage() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-lg w-full mx-auto flex flex-col gap-2"
-    >
-      <div className="mb-5">
-        <h1 className="mx-auto text-center text-2xl font-semibold">Login</h1>
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="email"
-          className="block mb-2.5 text-sm font-medium text-heading"
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="border border-black max-w-md w-full p-10 rounded-lg">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="max-w-lg w-full mx-auto flex flex-col gap-2"
         >
-          Your Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          {...register("email")}
-          className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
-        />
-        {formState.errors.email && (
-          <p className="text-red-600">{formState.errors.email.message}</p>
-        )}
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="password"
-          className="block mb-2.5 text-sm font-medium text-heading"
-        >
-          Password
-        </label>
-        <input
-          className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
-          type="password"
-          id="password"
-          placeholder="Password"
-          {...register("password")}
-        />
-        {formState.errors.password && (
-          <p className="text-red-600">{formState.errors.password.message}</p>
-        )}
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-brand box-border border border-white rounded-lg shadow-xs font-medium rounded-base text-sm px-4 py-2.5 focus:outline-none"
-      >
-        Login
-      </button>
+          <div className="mb-5">
+            <h1 className="mx-auto text-center text-2xl font-semibold">
+              Login
+            </h1>
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2.5 text-sm font-medium text-heading"
+            >
+              Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              {...register("email")}
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
+            />
+            {formState.errors.email && (
+              <p className="text-red-600">{formState.errors.email.message}</p>
+            )}
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2.5 text-sm font-medium text-heading"
+            >
+              Password
+            </label>
+            <input
+              className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
+              type="password"
+              id="password"
+              placeholder="Password"
+              {...register("password")}
+            />
+            {formState.errors.password && (
+              <p className="text-red-600">
+                {formState.errors.password.message}
+              </p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="text-black bg-brand box-border border border-black rounded-lg shadow-xs font-medium rounded-base text-sm px-4 py-2.5 focus:outline-none"
+          >
+            Login
+          </button>
 
-      <button
-        onClick={handleGoogleSignIn}
-        type="button"
-        className="text-white bg-brand box-border border border-white rounded-lg hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none "
-      >
-        Continue with Google
-      </button>
-      {errors && <p className="text-red-600">{errors}</p>}
-    </form>
+          <button
+            onClick={handleGoogleSignIn}
+            type="button"
+            className="text-black bg-brand box-border border border-blackrounded-lg  focus:ring-4  shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none "
+          >
+            Continue with Google
+          </button>
+          {errors && <p className="text-red-600">{errors}</p>}
+        </form>
+      </div>
+    </div>
   );
 }
--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,189 +1,32 @@
 'use client'
+import { useOnboarding } from '@/hooks/useOnboarding'
+import Step1 from './Step1';
+import Step2 from './Step2';
+import Step3 from './Step3';
+import Step4 from './Step4';
+import { useUser } from '@/hooks/useUser';
+import { useRouter } from 'next/navigation';
 
-import useDebounce from "@/hooks/useDebounce";
-import { useUser } from "@/hooks/useUser";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import {useForm} from 'react-hook-form'
-
-type FormData = {
-    headline: string;
-    bio: string;
-    genre_preference: string[];
-    name: string
-};
-
-export default function OnboardingPage(){
-  const genreRef = useRef<HTMLInputElement>(null);
-    const {user} = useUser();
-    console.log("user in onbarding is", user)
-    const router = useRouter();   
-    const {register, handleSubmit, setValue, watch }= useForm<FormData>({
-        defaultValues: {
-            headline: user?.headline || '',
-            bio: user?.bio || '',
-            genre_preference : user?.genre_preference|| [],
-            name: user?.name || ''
-        }
-    })
-    const genre = watch('genre_preference');
-    const nameValue = watch('name')
-    const debouncedName = useDebounce(nameValue, 1000)
-    const [errors, setErrors] = useState<string>('');
-
-    useEffect(() => {
-     const isAvailable = async () => {
-       const namecheck = await fetch(`/api/checkname?username=${debouncedName}`)
-       const isNameTaken = await namecheck.json()
-       if(isNameTaken.nameTaken){
-        setErrors("Name already taken try another username")
-       }else{
-        setErrors('')
-       }
-
-     }
-     isAvailable()
-     return () => {}
-    }, [debouncedName])
-
-    const addGenre = () => {
-      const val = genreRef.current?.value.trim();
-      if(!val) return;
-      setValue('genre_preference', [...genre, val]);
-      genreRef.current!.value = '';
-    }
-
-    const removeGenre = (item: string) => {
-      setValue('genre_preference', genre.filter(g => g !== item));
-    }
-    
-    const onSubmit = async (data: FormData) => {
-      const namecheck = await fetch(`/api/checkname?username=${debouncedName}`)
-      const isNameTaken = await namecheck.json()
-      if(isNameTaken.nameTaken){
-        console.log(isNameTaken.nameTaken)
-        setErrors("Name already taken try another username")
-        return
-      }
-      setErrors('')
-      console.log(isNameTaken.nameTaken)
-        const res = await fetch('/api/onboarding', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },            
-            body: JSON.stringify({...data, name: debouncedName, onboarding: true}),
-        })
-        if(res.ok){
-          console.log("--------ok")
-            // router.push('/leader/createClub')
-        }else{
-            const error = await res.json()
-            setErrors(error.message)
-        }
-    }
-    return (
-      <div>
-        <h1>Welcome to ReadRelay</h1>
-        <p>Please complete following details to get Started</p>
-        <div>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="max-w-lg mx-auto w-full flex flex-col gap-2"
-          >
-            <div className="mb5">
-              <label
-                htmlFor="name"
-                className="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                {...register("name")}
-                className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
-              />
-              {errors && <p className="text-red-500">{errors}</p>}
-            </div>
-            <div className="mb-5">
-              <label
-                htmlFor="headline"
-                className="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Tell us in short about yourself
-              </label>
-              <input
-                type="text"
-                {...register("headline")}
-                className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
-              />
-            </div>
-
-            <div className="mb-5">
-              <label
-                htmlFor="bio"
-                className="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Your Bio
-              </label>
-              <textarea
-                id="bio"
-                className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
-                {...register("bio")}
-              ></textarea>
-              <label
-                htmlFor="genre"
-                className="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Genre Preference
-              </label>
-              <input
-                type="text"
-                id="genre"
-                ref={genreRef}
-                placeholder="Enter your Genre here"
-                className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body rounded-lg"
-                onKeyDown={(e) => {
-                  if(e.key === 'Enter'){
-                    e.preventDefault();
-                    addGenre();
-                  }
-                }}
-              />
-              {genre?.length > 0 && (
-                            <div className="flex flex-wrap gap-2 mt-2">
-                                {genre.map((g, index) => (
-                                    <button
-                                        type="button"
-                                        key={index}
-                                        onClick={() => removeGenre(g)}
-                                        className="border border-white rounded-lg px-2 py-1 text-sm text-white flex items-center gap-1"
-                                    >
-                                        {g} <span>✕</span>
-                                    </button>
-                                ))}
-                            </div>
-                        )}
-            </div>
-
-            <div>
-              <h2>Let's get started</h2>
-              <button
-                type="submit"
-                className="text-white bg-brand box-border border border-white rounded-lg hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
-              >
-                Create Your first club
-              </button>
-              <button
-                type="button"
-                onClick={() => router.push("/dashboard")}
-                className="text-white bg-brand box-border border border-white rounded-lg hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
-              >
-                Skip for now, go to dashboard
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
+const OnboardingForm = () => {
+  const router = useRouter();
+  const {user, loading} = useUser();
+  const {step} = useOnboarding();
+  
+  
+  console.log("user is ", "-------------", user)
+  if(!user){
+    router.push('/login');
+  } 
+  if(user?.onboarding_complete) router.push('/dashboard');
+  if (loading) return <div>Loading...</div>;
+  return (
+    <div>
+      {step === 1  && <Step1/>}
+      {step === 2  && <Step2/>}
+      {step === 3  && <Step3/>}
+      {step === 4  && <Step4/>}
+    </div>
+  )
 }
+
+export default OnboardingForm
